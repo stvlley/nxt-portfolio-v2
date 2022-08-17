36bd43fc51bc6718a4efcf89d4ca31d2790df320
Stephen <stvlley.amzn@gmail.com>
2022-08-17 14:17:06 -0400

    lets fuckin goooo

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,55 +1,58 @@
 import {AiFillHtml5, } from 'react-icons/ai'
 import {DiCss3 } from 'react-icons/di'
 import {Parallax, ParallaxLayer }from '@react-spring/parallax'
+import Image from 'next/image'
 export default function Skills() {
   return (
         <>
-        <Parallax pages={2}>
 
         <div name='skills' className='bg-skills-img  w-full h-[600px] text-white'>
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col  justify-center w-full h-full'>
-          <ParallaxLayer>
           <div className=' p-4 hover:scale-110 duration-500 '>
               <p className='rounded-lg text-4xl font-bold inline'>Skills</p>
           </div>
-          </ParallaxLayer>
       
 
-          <ParallaxLayer offset={1}>
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='rounded-lg pt-3 bg-white  hover:scale-110 duration-500'>
+          <div className='w-full text-gray-700 grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className=' rounded-lg pt-3 shadow-lg bg-white hover:scale-110 duration-500'>
+                  <Image src='/html.svg' alt='' width={72} height={50} />
                   <p className='my-4'>HTML</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md bg-white hover:scale-110 duration-500'>
+              <Image src='/css.svg' alt='' width={72} height={50} />
                   <p className='my-4'>CSS</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
+              <Image src='/javascript.svg' alt='' width={72} height={50} />
                   <p className='my-4'>JAVASCRIPT</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
+              <Image src='/react-dark.svg' alt='' width={72} height={50} />
                   <p className='my-4'>REACT</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
+              <Image src='/rails.svg' alt='' width={72} height={50} />
                   <p className='my-4'>RAILS</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
-                  <p className='my-4'>TAILWIND</p>
+              <Image src='/nextjs-dark.svg' alt='' width={72} height={50} />
+                  <p className='my-4'>NEXTJS</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
-                  <p className='my-4'>SUPABASE</p>
+              <Image src='/tailwindcss-dark.svg' alt='' width={72} height={50} />
+                  <p className='my-4'>TAILWINDCSS</p>
               </div>
               <div className='rounded-lg pt-3 shadow-md  bg-white hover:scale-110 duration-500'>
-                  <p className='my-4'>NEXT</p>
+              <Image src='/python-light.svg' alt='' width={72} height={50} />
+                  <p className='my-4'>PYTHON</p>
               </div>
               
           </div>
-          </ParallaxLayer>
   
       </div>
     </div>
-    </Parallax>
     </>
   )
 }
